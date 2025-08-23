fba8883c4e9897aed231d4d23371603f8e9e8304
thaiguer <thaiguer.k@gmail.com>
2025-08-22 23:24:52 -0300

    add TokenService

---
+++ JwtAspNet/Services/TokenService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace JwtAspNet.Services;
+
+public class TokenService
+{
+    public string CreateToken()
+    {
+        var byteKey = Encoding.ASCII.GetBytes(Configuration.PrivateKey);
+        var symmetricSecurityKey = new SymmetricSecurityKey(byteKey);
+        var signingCredentials = new SigningCredentials(symmetricSecurityKey, SecurityAlgorithms.HmacSha256);
+        var securityTokenDescriptor = new SecurityTokenDescriptor
+        {
+            SigningCredentials = signingCredentials,
+            Expires = DateTime.UtcNow.AddHours(12)
+        };
+
+        var handler = new JwtSecurityTokenHandler();
+        var token = handler.CreateToken(securityTokenDescriptor);
+        return handler.WriteToken(token);
+    }
+}
