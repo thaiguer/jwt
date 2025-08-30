23628b839486fe98c3f1e4e8bce5f990fe678ff4
thaiguer <thaiguer.k@gmail.com>
2025-08-30 20:25:10 -0300

    simple test working

--- JwtAspNet/Program.cs
+++ JwtAspNet/Program.cs
@@ -1,15 +1,37 @@
+using JwtAspNet;
 using JwtAspNet.Models;
 using JwtAspNet.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<TokenService>();
 
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}
+).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.PrivateKey)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
 
-app.MapGet("/", GetToken);
-
+app.MapGet("/", () => "Hello");
+app.MapGet("/login", GetToken);
+app.MapGet("/restrito", () => "Segredo").RequireAuthorization();
 app.Run();
-
 string GetToken (TokenService tokenService)
 {
     var user = new User(
@@ -21,4 +43,9 @@ string GetToken (TokenService tokenService)
         new[] {"student", "premium" });
     
     return tokenService.CreateToken(user);
+}
+
+string Page()
+{
+    return "Página secreta";
 }
