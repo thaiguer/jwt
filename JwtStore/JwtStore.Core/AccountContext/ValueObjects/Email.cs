cd821073cf4e655ede1136cedb2bdbe853f518cb
thaiguer <thaiguer.k@gmail.com>
2025-09-19 21:02:38 -0300

    add file

--- JwtStore/JwtStore.Core/AccountContext/ValueObjects/Email.cs
+++ JwtStore/JwtStore.Core/AccountContext/ValueObjects/Email.cs
@@ -1,12 +1,52 @@
 ﻿using JwtStore.Core.SharedContext.Extensions;
 using JwtStore.Core.SharedContext.ValueObjects;
+using System.Text.RegularExpressions;
 
 namespace JwtStore.Core.AccountContext.ValueObjects;
 
-public class Email : ValueObject
+public partial class Email : ValueObject
 {
     private const string Pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
 
+    public Email(string address)
+    {
+        if(string.IsNullOrEmpty(address))
+        {
+            throw new Exception("address cant be null or empty");
+        }
+
+        address = address.Trim().ToLower();
+
+        if(address.Length < 5)
+        {
+            throw new Exception("address cant shorter than 5 char");
+        }
+
+        if (!EmailRegex().IsMatch(address))
+        {
+            throw new Exception("address does not match an e-mail format");
+        }
+
+        Address = address;
+    }
+
     public string Address { get; }
     public string Hash => Address.ToBase64();
+
+    [GeneratedRegex(Pattern)]
+    private static partial Regex EmailRegex();
+
+    #region Implicit Operators
+    
+    public static implicit operator string(Email email)
+    {
+        return email.ToString();
+    }
+
+    public static implicit operator Email(string address)
+    {
+        return new Email(address);
+    }
+
+    #endregion
 }
